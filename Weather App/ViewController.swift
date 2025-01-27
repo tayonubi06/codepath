6de02ad8b9e58bd167ee3f4b5e3602e4f83f992d
tayonubi06 <tayoshonubi@outlook.com>
2025-01-27 01:20:11 -0600

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Omotayo Joseph Shonubi on 27/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        let randomColor = changeColor()
+           view.backgroundColor = randomColor
+    }
+    
+    
+}
+
